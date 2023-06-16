dccf7799a8de4568070d1f4d516fca035ec7e3af
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-06-16 08:40:49 +0530

    Html form style changes

--- application.py
+++ application.py
@@ -63,6 +63,6 @@ def predict_cancer():
         raise CustomException(e,sys)
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=8080,debug=True)
 
 
